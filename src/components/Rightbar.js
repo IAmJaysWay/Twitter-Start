c288eecb94fc4349cf0b101a13a3aabb96cdd1bf
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-04-19 12:38:36 +0300

    Update Rightbar.js

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -3,7 +3,33 @@ import './Rightbar.css';
 
 
 const Rightbar = () => {
-  
+  const trends = [
+    {
+      img: spaceshooter,
+      text: "Learn how to build a Web3 FPS game using unity...",
+      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-space-fps-game/",
+    },
+    {
+      img: netflix,
+      text: "The fisrt Moralis Project! Let's Netflix and chill...",
+      link: "https://moralis.io/moralis-projects-learn-to-build-a-web3-netflix-clone/",
+    },
+    {
+      img: academy,
+      text: "Master DeFi in 2022. Start  at the Moralis Academy...",
+      link: "https://academy.moralis.io/courses/defi-101",
+    },
+    {
+      img: js,
+      text: "Become a Web3 Developer with just simple JS...",
+      link: "https://academy.moralis.io/all-courses",
+    },
+    {
+      img: youtube,
+      text: "Best youtube channel to learn about Web3...",
+      link: "https://www.youtube.com/channel/UCgWS9Q3P5AxCWyQLT2kQhBw",
+    },
+  ];
 
   return (
     <>
